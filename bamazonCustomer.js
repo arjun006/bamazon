dc2280ef25425f1f25079a1a2487d9985439494d
arjun006 <arjun.devakumar66@gmail.com>
2018-12-19 22:21:20 -0500

    inquirer changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,11 +16,35 @@ connection.connect(function(err) {
 });
 
 function afterConnection() {
-  connection.query("SELEnCT * FROM products", function(err, res) {
+  connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
     if (err) throw err;
     console.log(res);
     
   });
+  connection.end();
 };
 
-connection.end();
+inquirer.prompt([{
+  name:"item_id",
+  type:"input",
+  message:"What is the ID of the product you would like to buy?\""
+},
+{
+  name:"stock_quantity",
+  type:"input",
+  message:"How many do you want to purchase?"
+}
+]).then(function(product){
+  var product = product.item_id;
+  var quantity = product.stock_quantity;
+
+  console.log(product);
+  console.log(quantity);
+
+  var query = "SELECT * FROM products WHERE ?";
+  connection.query(query,{item_id:item}, function(err,data){
+  })
+
+})
+
+
